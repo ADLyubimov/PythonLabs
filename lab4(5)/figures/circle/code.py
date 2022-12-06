81a920d16b6355c674fc57db4dfdfb3efbe9f11b
ADLyubimov <113112190+ADLyubimov@users.noreply.github.com>
2022-12-06 21:21:31 +0300

    Update code.py

---
+++ lab4(5)/figures/circle/code.py
@@ -0,0 +1,9 @@
+default_radius = 5
+
+#Периметр
+def circle_perimeter(radius=default_radius):
+    return (2*radius*3,14)
+    
+#Площадь
+def circle_area(radius=default_radius):
+    return (3,14*(radius**2))
